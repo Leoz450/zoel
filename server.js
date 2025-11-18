cd7c4839821078aecdbd98c3c269dd7c178ab227
Leoz450 <l.oikonomou450@gmail.com>
2025-11-18 13:06:13 +0200

    Update API endpoint to v1beta for content generation

--- server.js
+++ server.js
@@ -51,8 +51,8 @@ app.post("/api/chat", async (req, res) => {
     const body = { contents };
 
     const fetchRes = await fetch(
-      "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash-latest:generateContent?key=" +
-        GEMINI_API_KEY,
+  `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${GEMINI_API_KEY}`,
+
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
